63e92fff5ae56e9f9cdf59363c904042868a45d8
ejioguvictor <ejioguvictor6054@gmail.com>
2022-02-01 11:18:47 +0100

    modified getMessages and getGroupMessages route handlers

---
+++ src/controllers/messageController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { Message } from '../models/MessageModel';
+
+/* Route for getting all the messages for a private chat */
+export async function getMessages(req: Request, res: Response) {
+  try {
+    const messages = await Message.find({ chatId: req.params.chatId });
+    res.status(200).json({ messages });
+  } catch (error) {
+    res.status(404).json({ error: 'Unable to get group messages' });
+  }
+}
+
+/* Route for getting all the messages for a specific chat-room
+ specified by the query parameter room */
+export async function getGroupMessages(req: Request, res: Response) {
+  try {
+    let id = req.params.id;
+    const chatGroup = Message.find({ id, chatType: 'Group' });
+    if (!chatGroup) {
+      return res.status(404).json({ message: 'Group does not exist!' });
+    }
+    res.status(200).json({ data: chatGroup });
+  } catch (error) {
+    res.status(500).json({ error: 'Not available' });
+  }
+}
--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import {
+  getGroupMessages,
+  getMessages,
+} from '../controllers/messageController';
 
 const router = express.Router();
 
 // routers
-
-
+router.get('./:chatId', getMessages);
+router.get('./group/:chatId', getGroupMessages);
 
 // exported router
 export default router;
