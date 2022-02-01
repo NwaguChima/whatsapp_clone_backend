f5c1081bd444fa7461a3e43563960e59aed010ab
chei7tain <fredrickokwuobi@gmail.com>
2022-02-01 17:05:57 +0100

    add getAllGroups controller where the user is a member

--- src/routes/groupRoutes.ts
+++ src/routes/groupRoutes.ts
@@ -1,23 +1,18 @@
 import express from 'express';
 import app from '../app';
+import { getAllGroups } from '../controllers/get_groups.controller';
+import { protect } from '../controllers/verifyEmail';
 
 const router = express.Router();
 
 // router
 
-
 // get route
-router.route("/").get();
+// router.route('/').get();
 
-
+router.route('/').get(protect,getAllGroups);
 // posts route
-router.route("/").post();
-
-
-
-
-
-
+router.route('/').post();
 
 // exported router
 export default router;
