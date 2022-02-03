75514d3c81b0f83bec7ca9b6f34d7cd6ee835041
chei7tain <fredrickokwuobi@gmail.com>
2022-02-03 20:21:10 +0100

    add get friend controller

--- src/controllers/get_favourite_controller.ts
+++ src/controllers/get_favourite_controller.ts
@@ -1,2 +1,37 @@
-// import {Response} from "express"
-// import {Favorite} from "../models/FavoriteModel";
+import { Response } from 'express';
+import { CustomRequest } from '../utils/custom';
+import { UserAuth } from '../models/Users';
+import { Friend } from '../models/userFriendModel';
+
+const getFavouriteFriends = async (req: CustomRequest, res: Response) => {
+  try {
+    const queryId = req.query.friendId;
+    const { _id } = req.user;
+    const userId = req.user.id;
+    // console.log(req.query);
+    // let id = '61f966a8a9bfac9a30be797a';
+    let query = await UserAuth.find({ _id: userId });
+    let ids = query[0].favoriteFriends;
+    // find favourite friends of the user
+    let friends = await Friend.find({})
+      .populate('friendId')
+      .where('friendId')
+      .in(ids);
+
+    if (friends.length === 0) {
+      res.status(200).json({
+        message: 'No friends found',
+      });
+    } else {
+      res.status(200).json({
+        message: 'success',
+        length: friends.length,
+        data: friends,
+      });
+    }
+  } catch (err: any) {
+    console.log(err);
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+export { getFavouriteFriends };
